dba9bc526b5819ebce764fa22017e9b31ced63cf
makocp <mako.codeproducer@gmail.com>
2023-06-17 10:21:45 +0200

    refactored MainNavGraph

--- app/src/main/java/com/example/carpoint/MainActivity.kt
+++ app/src/main/java/com/example/carpoint/MainActivity.kt
@@ -4,15 +4,7 @@ import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.carpoint.Screens.Dashboard.DashboardScreen
-import com.example.carpoint.Screens.LogIn.LogInScreen
-import com.example.carpoint.Screens.ResetPassword.ResetPassword
-import com.example.carpoint.Screens.ResetPassword.ResetPasswordTransmission
-import com.example.carpoint.Screens.SignUp.SignUpScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -25,13 +17,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             val navController = rememberNavController()
 
-            NavHost(navController = navController, startDestination = "login") {
-                composable("login") { LogInScreen(navController) }
-                composable("signup") { SignUpScreen(navController) }
-                composable("resetpasswordTransmission") {ResetPasswordTransmission(navController)}
-                composable("resetpassword"){ResetPassword(navController)}
-                composable("dashboard") { DashboardScreen() }
-            }
+            MainNavGraph(navController = navController)
         }
     }
 }
---
+++ app/src/main/java/com/example/carpoint/MainNavGraph.kt
@@ -0,0 +1,22 @@
+package com.example.carpoint
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.carpoint.Screens.Dashboard.DashboardScreen
+import com.example.carpoint.Screens.LogIn.LogInScreen
+import com.example.carpoint.Screens.ResetPassword.ResetPassword
+import com.example.carpoint.Screens.ResetPassword.ResetPasswordTransmission
+import com.example.carpoint.Screens.SignUp.SignUpScreen
+
+@Composable
+fun MainNavGraph(navController: NavHostController){
+    NavHost(navController = navController, startDestination = "login") {
+        composable("login") { LogInScreen(navController) }
+        composable("signup") { SignUpScreen(navController) }
+        composable("resetpasswordTransmission") { ResetPasswordTransmission(navController) }
+        composable("resetpassword"){ ResetPassword(navController) }
+        composable("dashboard") { DashboardScreen() }
+    }
+}
