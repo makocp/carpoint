bf6f6574df5402395b054628042867f5a2ac9d86
makocp <mako.codeproducer@gmail.com>
2023-06-18 00:21:15 +0200

    Photo Picker added

--- app/src/main/java/com/example/carpoint/Screens/Dashboard/HomeScreen.kt
+++ app/src/main/java/com/example/carpoint/Screens/Dashboard/HomeScreen.kt
@@ -6,16 +6,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
-import coil.compose.rememberImagePainter
-import com.example.carpoint.R
 
 @Composable
 fun HomeScreen() {
@@ -37,14 +33,18 @@ fun HomeScreen() {
 @Preview
 @Composable
 fun ProfileView() {
-    // TODO: Hier von Storage Uri/Image reinholen.
-    val imageUri = rememberSaveable { mutableStateOf("") }
-    val painter = rememberImagePainter(
-        if (imageUri.value.isEmpty()) {
-            R.drawable.person_placeholder
-        } else {
-            imageUri.value
-        }
-    )
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Blue),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Home",
+            fontSize = 100.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
 }
+
