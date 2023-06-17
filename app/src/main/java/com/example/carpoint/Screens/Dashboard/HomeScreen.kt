3b4e9df3f16ec9a69b7d64ce03ec1c46645366c2
makocp <mako.codeproducer@gmail.com>
2023-06-17 16:07:19 +0200

    started profileView Logic

--- app/src/main/java/com/example/carpoint/Screens/Dashboard/HomeScreen.kt
+++ app/src/main/java/com/example/carpoint/Screens/Dashboard/HomeScreen.kt
@@ -6,12 +6,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberImagePainter
+import com.example.carpoint.R
 
 @Composable
 fun HomeScreen() {
@@ -25,6 +29,22 @@ fun HomeScreen() {
         Text(
             text = "Home",
             fontSize = 100.sp,
-            fontWeight = FontWeight.Bold)
+            fontWeight = FontWeight.Bold
+        )
     }
+}
+
+@Preview
+@Composable
+fun ProfileView() {
+    // TODO: Hier von Storage Uri/Image reinholen.
+    val imageUri = rememberSaveable { mutableStateOf("") }
+    val painter = rememberImagePainter(
+        if (imageUri.value.isEmpty()) {
+            R.drawable.person_placeholder
+        } else {
+            imageUri.value
+        }
+    )
+
 }
