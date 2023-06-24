74237c359af86d497a551f2a4317d0f5bbc6eab2
mxhxmmxd <mohamed.alamer@edu.fh-joanneum.at>
2023-06-24 17:24:44 +0200

    settings screen should be finished

--- app/src/main/java/com/example/carpoint/screens/dashboard/SettingsScreen/SettingsScreen.kt
+++ app/src/main/java/com/example/carpoint/screens/dashboard/SettingsScreen/SettingsScreen.kt
@@ -4,48 +4,60 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.example.carpoint.R
+import com.example.carpoint.screens.dashboard.SettingsScreen.SettingsViewModel
+import com.example.carpoint.utils.CreateButton
+import kotlinx.coroutines.launch
 
 @Composable
-fun SettingsScreen(navController: NavHostController, navControllerMain: NavHostController){
-
+fun SettingsScreen(
+    navController: NavHostController,
+    navControllerMain: NavHostController,
+    viewmodel: SettingsViewModel = hiltViewModel()
+) {
+    val scope = rememberCoroutineScope()
     val loginSharedPref =
         LocalContext.current.getSharedPreferences(
             com.example.carpoint.sharedPreferences.SharedPreferences.LOGIN_PREF.prefName,
             0
         )
+    val email = loginSharedPref.getString("email", null)
+
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Yellow),
+            .background(Color.White),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "Settings",
-            fontSize = 100.sp,
-            fontWeight = FontWeight.Bold
-        )
-        Button(
-            onClick = {
-                with (loginSharedPref.edit()){
-                    clear()
-                    apply()
+        CreateButton(placeholderResId = R.string.changepassword) {
+            scope.launch {
+                if (email != null) {
+                    viewmodel.changePassword(email)
                 }
-                navControllerMain.navigate("login")
             }
-        ) {
-            Text(text = "Logout")
+        }
+        CreateButton(placeholderResId = R.string.signout) {
+            with(loginSharedPref.edit()) {
+                clear()
+                apply()
+            }
+
+            navControllerMain.navigate("login") {
+                // Pop up to the login screen and clear the back stack
+                popUpTo("login") {
+                    inclusive = true
+                }
+            }
         }
     }
 }
